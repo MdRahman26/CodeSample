d8c610e0c4d250c8a797a16e5f61cc1adfd13cb1
Mahmudul Hasan <mahmudbd90@gmail.com>
2018-05-18 11:24:50 -0400

    code sample updated

--- RestApi/TestApiStatusCode.java
+++ RestApi/TestApiStatusCode.java
@@ -16,7 +16,7 @@ public class TestApiStatusCode extends ApiStatusCode {
     // Testing the Status Code: 200 ok
     @Test(groups = "apiTestApiStatusCode", priority = 1)
     public void givenURIExists_whenResponseReceived() {
-        //TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
 
         Response response = given()                                             // content-type, cookies, params
                             .when().get(apiHome + "AllEmployeeResources")       // http method type
@@ -24,9 +24,7 @@ public class TestApiStatusCode extends ApiStatusCode {
 
         String status = response.getStatusLine(); // HTTP/1.1 200 OK
         String body = response.getBody().print();
-
         Assert.assertEquals(response.getStatusCode(), 200);
-
         System.out.println(status);
         System.out.println("TC1 Passed");
     }
@@ -34,11 +32,13 @@ public class TestApiStatusCode extends ApiStatusCode {
     // Testing the Status Code: 404 not found
     @Test(groups = "apiTestApiStatusCode", priority = 2)
     public void givenURIDoesNotExist_whenResponseReceived() throws IOException {
-        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
+        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
         String randomUri = RandomStringUtils.randomAlphabetic(12);
-        Response response = given().when().get(apiHome + randomUri)
-                .then().statusCode(404).extract().response();
+
+        Response response = given()
+                            .when().get(apiHome + randomUri)
+                            .then().statusCode(404).extract().response();
+
         String status = response.getStatusLine();
         String body = response.getBody().print();
         Assert.assertEquals(response.getStatusCode(), 404);
@@ -49,12 +49,15 @@ public class TestApiStatusCode extends ApiStatusCode {
     // Testing the Status Code for all valid uri
     @Test(groups = "apiTestApiStatusCode", priority = 3)
     public void givenURIsExist_whenResponseReceived() throws IOException {
-        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
+        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
         List<String> validUriList = getValidUriList();
+
         for (String validUri : validUriList) {
-            Response response = given().when().get(apiHome + validUri)
-                    .then().extract().response();
+
+            Response response = given()
+                                .when().get(apiHome + validUri)
+                                .then().extract().response();
+
             String status = response.getStatusLine();
             System.out.println(apiHome + validUri);
             System.out.println(status);
@@ -65,12 +68,15 @@ public class TestApiStatusCode extends ApiStatusCode {
     // Testing the Status Code for five invalid uri
     @Test(groups = "apiTestApiStatusCode", priority = 4)
     public void givenURIsDoNotExist_whenResponseReceived() throws IOException {
-        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName()));
+        TestLogger.log(convertToString("C: " + getClass().getSimpleName()) + " - M: " + convertToString(new Object() {}.getClass().getEnclosingMethod().getName()));
         List<String> invalidUriList = getInvalidUriList();
+
         for (String invalidUri : invalidUriList) {
-            Response response = given().when().get(apiHome + invalidUri)
-                    .then().extract().response();
+
+            Response response = given()
+                                .when().get(apiHome + invalidUri)
+                                .then().extract().response();
+
             String status = response.getStatusLine();
             System.out.println(apiHome + invalidUri);
             System.out.println(status);
